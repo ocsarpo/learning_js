4e5b8dc51fa25a18a0bd186202b159181aaed82d
ocsarpo <ocsarpo22@gmail.com>
2026-03-05 23:55:16 +0900

    변수

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
-// 코드러너 익스텐션으로 실행할 수 있어요. 오른쪽 상단 재생버튼
-console.log("Hello Code Runner!1");
-console.log("Hello Code Runner!2");
-console.log("Hello Code Runner!3");
-console.log("Hello Code Runner!4");
+var str = 'Hello Code Runner!';
 
-// 설정 -> code runner -> clear previous output -> true : 실행할때마다 클리어하고 출력함
+console.log(str);
+console.log(str);
+console.log(str);
+console.log(str);
