c764439c6316518b22827b97a57284cd43a7309a
ocsarpo <ocsarpo22@gmail.com>
2026-03-06 00:05:40 +0900

    let과 const

--- main.js
+++ main.js
@@ -13,3 +13,36 @@ console.log(str);
 // 10 + 20 -> 표현식.
 // 표현식의 결과 30 -> 값
 var sum = 10 + 20;
+
+/**
+ * let 과 const 키워드.
+ * ---- AI 자동완성 START----
+ * var : ES5 에서 사용하던 변수 선언 키워드.
+ * let : ES6 에서 사용하는 변수 선언 키워드.
+ * const : ES6 에서 사용하는 상수 선언 키워드.
+ * 
+ * var, let, const 차이점.
+ * 
+ * var : 함수 스코프.
+ * let, const : 블록 스코프.
+ * 
+ * var : 재선언 가능.
+ * let, const : 재선언 불가능.
+ * 
+ * var : 재할당 가능.
+ * let : 재할당 가능.
+ * const : 재할당 불가능.
+ * 
+ * var : 초기화 전에 사용 가능.
+ * let, const : 초기화 전에 사용 불가능.
+ * 
+ * var : undefined.
+ * let, const : ReferenceError.
+ * 
+ * var : 런타임 에러.
+ * let, const : 컴파일 에러.
+ * 
+ * var : 호이스팅이 발생.
+ * let, const : 호이스팅이 발생하지 않음.
+ * ---- AI 자동완성 END ----
+ */
