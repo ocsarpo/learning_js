8ae0844a9d7111b713ab46138c3b6dc064784042
ocsarpo <ocsarpo22@gmail.com>
2026-03-06 00:00:17 +0900

    변수와 연산자 등 용어정리

--- main.js
+++ main.js
@@ -4,3 +4,12 @@ console.log(str);
 console.log(str);
 console.log(str);
 console.log(str);
+
+// var -> 키워드 (변수 선언 시 사용. 따라서 sum 은 변수)
+// = -> 할당 연산자.
+// 10 -> 피연산자.
+// + -> 연산자.
+// 20 -> 피연산자.
+// 10 + 20 -> 표현식.
+// 표현식의 결과 30 -> 값
+var sum = 10 + 20;
