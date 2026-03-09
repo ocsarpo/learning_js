768ea2b2a5f3d3b667f1e1a844110db523cc8100
ocsarpo <ocsarpo22@gmail.com>
2026-03-09 13:57:28 +0900

    함수

---
+++ function.js
@@ -0,0 +1,57 @@
+/**
+ * 가변인자 함수.
+ */
+function sum() {
+    // 유사배열객체
+    console.log(arguments);
+
+    let result = 0;
+    for (let value of arguments) {
+        result = result + value;
+    }
+    console.log(result);
+}
+
+sum(10, 20);
+sum(10, 20, 30, 40);
+sum(10);
+
+function sum2(...args) {
+    // 배열
+    console.log(args);
+
+    let result = 0;
+    for (let value of args) {
+        result = result + value;
+    }
+    console.log(result);
+}
+
+sum2(10, 20);
+sum2(10, 20, 30, 40);
+sum2(10);
+
+function sum3(first, second, ...args) {
+    console.log(first);
+    console.log(second);
+    console.log(args);
+}
+
+sum3(10, 20);
+sum3(10, 20, 30, 40);
+sum3(10);
+
+/**
+ * return
+ */
+function sum4(first, second, ...args) {
+    let result = first + second;
+    for (let value of args) {
+        result = result + value;
+    }
+    return result;
+}
+
+console.log(sum4(10, 20));
+console.log(sum4(10, 20, 30, 40));
+console.log(sum4(10));
