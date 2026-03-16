95bf9ffdedb277ef44a075e93940d8a77db2595c
oscar <ocsarpo22@gmail.com>
2026-03-16 23:03:35 +0900

    콜백

--- syncAndAsync.js
+++ syncAndAsync.js
@@ -23,5 +23,44 @@ receiveB()
 receiveC()
 
 /**
- * 콜백.
+ * 콜백함수: 다른함수의 매개변수로 사용되는 함수.
  */
+function first(callback) {
+    console.log('first');
+    callback();
+}
+
+first(function () {
+    console.log('second');
+})
+
+function second() {
+    console.log('second');
+}
+first(second);
+
+first(second()); // second()가 먼저 실행되고 그 반환값을 first에 전달함. 콜백아님 주의!
+
+/**
+ * 비동기 순서제어. 철수 -> 영희 -> 민수 순서로 실행되도록.
+ */
+let sec2 = 1000;
+function receiveA() {
+    console.log("철수 하이");
+}
+
+function receiveB(callback) {
+    setTimeout(function () {
+        console.log("영희 하이");
+        callback();
+    }, sec2)
+}
+
+function receiveC() {
+    console.log("민수 하이");
+}
+
+receiveA();
+receiveB(receiveC);
+console.log("끝"); // 비동기 함수가 실행되는 동안 다른 코드들이 실행됨. -> 콜백함수를 사용해야 순서제어가 가능.
+
