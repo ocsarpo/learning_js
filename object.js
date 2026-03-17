f5bdd000108981b9e56150b44c2b5907f444065a
ocsarpo <ocsarpo22@gmail.com>
2026-03-18 00:27:54 +0900

    프로토타입

--- object.js
+++ object.js
@@ -31,8 +31,8 @@ console.log('-------');
 
 // new 없이 생성자 함수 호출하면 window 객체에 추가됨.
 Person2('오scar_window', 200);
-console.log(window.name); // 오scar_window
-console.log(window.age); // 200
+// console.log(window.name); // 오scar_window
+// console.log(window.age); // 200
 
 
 // 함수도 객체다.
@@ -42,4 +42,14 @@ console.dir(Person3);
 console.log(Person3.name);
 // 가변인자에 대해 배울 때 사용했었던 arguments의 정체는 함수 객체의 프로퍼티다.
 console.log(Person3.arguments);
+
+/**
+ * 프로토타입.
+ */
+console.log('-----------------------프로토타입-------------');
+// 함수와 프로토타입 객체는 1:1 관계를 가진다.
+console.dir(Person3);
+console.log(Person3.prototype);
+console.log(Person3.__proto__); // 프로토타입 내부 슬롯. 콘솔에서 [[Prototype]] 로 보임
+console.log(Person3.prototype.constructor); // Person3 함수 객체
 
