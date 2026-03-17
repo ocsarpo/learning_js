49e413599cb48fe7f588a52525dd38e7248f3f3d
ocsarpo <ocsarpo22@gmail.com>
2026-03-18 00:47:53 +0900

    프로토타입 활용

--- object.js
+++ object.js
@@ -79,3 +79,34 @@ console.log(protoTypeChain1.__proto__.constructor === ProtoTypeChain); // true
 // 알아서 체이닝 됨. this 가 바인딩 된다는 데 차이점은 다음강의
 console.log(protoTypeChain1.constructor === ProtoTypeChain); // true
 
+/**
+ * 프로토타입 활용
+ */
+console.log('-----------------------프로토타입 활용-------------');
+// name과 age 는 바뀔 수 있지만. introduce 함수는 바뀌지 않는다.
+// 객체에 중복된 속성이 있다는 것.
+// 중복된 코드를 프로토타입 객체에 등록하여 중복을 줄일 수 있다.
+// 프로토타입에 두면 객체에는 속성이 빠진다. __proto__ 로 접근하여 참조할 수 있기 때문에 전혀 문제없다.
+
+function ProtoTypeUtil(name, age) {
+    this.name = name;
+    this.age = age;
+}
+
+ProtoTypeUtil.prototype.introduce = function () {
+    console.log(`안녕하세요 ${this.name}입니다.`);
+};
+
+const protoTypeUtil1 = new ProtoTypeUtil('1', 21);
+const protoTypeUtil2 = new ProtoTypeUtil('2', 22);
+const protoTypeUtil3 = new ProtoTypeUtil('3', 23);
+const protoTypeUtil4 = new ProtoTypeUtil('4', 24);
+console.dir(protoTypeUtil1); // introduce 가 프로토타입 내부슬롯에 위치함을 알 수 있음
+console.log(protoTypeUtil1.__proto__ === ProtoTypeUtil.prototype); // true
+console.log(protoTypeUtil2.__proto__ === ProtoTypeUtil.prototype); // true
+console.log('-------------------------');
+console.log(protoTypeUtil2.__proto__.introduce()); // undefined -> 프로토타입객체에는 name, age 속성이 없어서 this.name 이 undefined
+console.log(protoTypeUtil2.introduce()); // 안녕하세요 2입니다. -> this 가 객체에 바인딩 됐기 때문에 name 접근 가능. 
+console.log('-----------------------------');
+console.log(protoTypeUtil2.__proto__.introduce === ProtoTypeUtil.prototype.introduce); // true
+console.log(protoTypeUtil1.introduce === protoTypeUtil2.introduce); // true
